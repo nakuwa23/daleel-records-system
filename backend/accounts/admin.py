1f0154d4ff30e45ac336086f44860ff5b025124d
nakuwa23 <b.nakuwa@alustudent.com>
2026-07-12 14:58:05 +0200

    Register Institution and custom User in Django admin

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Institution, User
 
-# Register your models here.
+
+@admin.register(Institution)
+class InstitutionAdmin(admin.ModelAdmin):
+    list_display = ("name", "location", "role", "created_at")
+    search_fields = ("name", "location")
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "role", "institution", "is_staff")
+    list_filter = ("role", "is_staff", "is_superuser")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Daleel", {"fields": ("role", "institution")}),
+    )
