4ef2b14bb71f9e2329f42726714298bec7def717
nakuwa23 <b.nakuwa@alustudent.com>
2026-07-12 15:50:54 +0200

    Register Learner in Django admin

--- backend/learners/admin.py
+++ backend/learners/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Learner
 
-# Register your models here.
+
+@admin.register(Learner)
+class LearnerAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "place_of_origin", "photo_source", "created_by", "created_at")
+    list_filter = ("photo_source", "place_of_origin")
+    search_fields = ("full_name", "place_of_origin", "guardian_info")
+    readonly_fields = ("learner_id", "created_at")
