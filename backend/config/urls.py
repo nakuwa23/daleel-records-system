59e151d90d48c697124be2a9751d0d8957781496
nakuwa23 <b.nakuwa@alustudent.com>
2026-07-12 18:16:54 +0200

    Wire up Learner API routes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from learners.views import LearnerViewSet
+
+router = DefaultRouter()
+router.register(r"learners", LearnerViewSet, basename="learner")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
